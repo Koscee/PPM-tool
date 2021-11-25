ee774d63a96be5f2ddbbd7a9bde39308ac8bf3bc
Koscee <koscee.anason@yahoo.com>
2021-11-25 17:36:31 +0800

    Add html and css code for Dashboard, ProjectItem and Header components.

--- ppmtool-react-client/src/components/layout/Header.js
+++ ppmtool-react-client/src/components/layout/Header.js
@@ -3,9 +3,44 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav className="navbar navbar-expand-md navbar-dark mb-4">
+        <div className="container">
+          <a className="navbar-brand mb-0 h1" href="Dashboard.html">
+            Personal Project Management Tool
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav ms-5 me-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/dashboard">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link " href="register.html">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
--- ppmtool-react-client/src/components/project/ProjectItem.js
+++ ppmtool-react-client/src/components/project/ProjectItem.js
@@ -2,7 +2,42 @@ import React, { Component } from "react";
 
 class ProjectItem extends Component {
   render() {
-    return <h2>Project Item</h2>;
+    return (
+      <div className="container project-item">
+        <div className="card card-body mb-3">
+          <div className="row">
+            <div className="col-2">
+              <span className="mx-auto">REACT</span>
+            </div>
+            <div className="col-lg-6 col-md-6 col-8">
+              <h3>Spring / React Project</h3>
+              <p>Project to create a Kanban Board with Spring Boot and React</p>
+            </div>
+            <div className="col-md-4 d-lg-block">
+              <ul className="list-group">
+                <a href="#">
+                  <li className="list-group-item board">
+                    <i className="fa fa-tasks pe-1">&nbsp; Project Board</i>
+                  </li>
+                </a>
+                <a href="#">
+                  <li className="list-group-item update">
+                    <i className="fa fa-edit pe-1">&nbsp; Update Project</i>
+                  </li>
+                </a>
+                <a href="">
+                  <li className="list-group-item delete">
+                    <i className="fa fa-trash-alt pe-1">
+                      &nbsp; Delete Project
+                    </i>
+                  </li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
