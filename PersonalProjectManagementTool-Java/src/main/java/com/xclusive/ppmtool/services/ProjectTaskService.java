7a20b7d691991c837df0afc5a6c8e5e0cf90f03f
Koscee <52253099+Koscee@users.noreply.github.com>
2021-12-07 19:14:15 +0800

    Handle project not found exception in ProjectTaskService, Addconfiguration for connecting with MYSQL database.

--- PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/services/ProjectTaskService.java
+++ PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/services/ProjectTaskService.java
@@ -4,7 +4,9 @@ import com.xclusive.ppmtool.domain.Backlog;
 import com.xclusive.ppmtool.domain.ProjectTask;
 import com.xclusive.ppmtool.enums.Priority;
 import com.xclusive.ppmtool.enums.Status;
+import com.xclusive.ppmtool.exceptions.ProjectNotFoundException;
 import com.xclusive.ppmtool.repositories.BacklogRepository;
+import com.xclusive.ppmtool.repositories.ProjectRepository;
 import com.xclusive.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,45 +22,59 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-        // Exceptions: Project not found
-        // Add PTs to a specific project,
+        // handle Exceptions: Project not found
+        try {
+            // Add PTs to a specific project,
             //  i.e check project != null and backlog exists
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
 
-        // set the backlog to projectTask
-        projectTask.setBacklog(backlog);
+            // set the backlog to projectTask
+            projectTask.setBacklog(backlog);
 
-        // projectSequence is the projectIdentifier + PTSequence (position of the task within the backlog)
+            // projectSequence is the projectIdentifier + PTSequence (position of the task within the backlog)
             // (eg: IDET-1, IDASF-2)
-        Integer BacklogSequence = backlog.getPTSequence();
+            Integer BacklogSequence = backlog.getPTSequence();
 
-        // increase PTSequence only when new projectTask is added and
-        // update and set the PTSequence (which is within the backlog)
-        BacklogSequence++;
+            // increase PTSequence only when new projectTask is added and
+            // update and set the PTSequence (which is within the backlog)
+            BacklogSequence++;
 
-        backlog.setPTSequence(BacklogSequence);
+            backlog.setPTSequence(BacklogSequence);
 
-        // Add Sequence to Project Task
-        String projectSequence = projectIdentifier+"-"+BacklogSequence;
-        projectTask.setProjectSequence(projectSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            // Add Sequence to Project Task
+            String projectSequence = projectIdentifier+"-"+BacklogSequence;
+            projectTask.setProjectSequence(projectSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        // set an INITIAL priority when priority is null
-        if (projectTask.getPriority() == null) {  // need this check "projectTask.getPriority() == 0" to handle form data in the future
-            projectTask.setPriority(Priority.LOW.getValue());
+            // set an INITIAL priority when priority is null
+            if (projectTask.getPriority() == null) {  // need this check "projectTask.getPriority() == 0" to handle form data in the future
+                projectTask.setPriority(Priority.LOW.getValue());
+            }
+
+            // set an INITIAL status when status is null
+            if ("".equals(projectTask.getStatus()) || projectTask.getStatus() == null) {
+                projectTask.setStatus(Status.TODO.getAction());
+            }
+
+            return projectTaskRepository.save(projectTask);
+
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project not found");
         }
-
-        // set an INITIAL status when status is null
-        if ("".equals(projectTask.getStatus()) || projectTask.getStatus() == null) {
-            projectTask.setStatus(Status.TODO.getAction());
-        }
-
-        return projectTaskRepository.save(projectTask);
 
     }
 
     public Iterable<ProjectTask> findBacklogBypId(String id) {
+
+        if (projectRepository.findByProjectIdentifier(id) == null) {
+            throw new ProjectNotFoundException("Project with ID: '"+id+"' does not exist");
+        }
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
+
+    
 }
