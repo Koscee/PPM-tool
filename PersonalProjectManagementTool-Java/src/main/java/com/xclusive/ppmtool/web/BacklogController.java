99c53a9f6230a8c44a8e77aa51bb5d0101fe2815
Koscee <52253099+Koscee@users.noreply.github.com>
2021-12-09 14:05:02 +0800

    Implement fetching projectTask by projectSequence (happy path).

--- PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/web/BacklogController.java
+++ PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/web/BacklogController.java
@@ -38,4 +38,10 @@ public class BacklogController {
     public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_pId) {
         return projectTaskService.findBacklogBypId(backlog_pId);
     }
+
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id) {
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
 }
