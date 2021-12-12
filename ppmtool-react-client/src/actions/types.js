029dc84b4ac068d72f527fa2704e767501b64e72
Koscee <52253099+Koscee@users.noreply.github.com>
2021-12-12 19:16:26 +0800

    Add action types and reducer for projectTask Backlog.

--- ppmtool-react-client/src/actions/types.js
+++ ppmtool-react-client/src/actions/types.js
@@ -3,3 +3,9 @@ export const REMOVE_ERRORS = "REMOVE_ERRORS";
 export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
+
+// Types for BACKLOG & PROJECT_TASK actions
+
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
