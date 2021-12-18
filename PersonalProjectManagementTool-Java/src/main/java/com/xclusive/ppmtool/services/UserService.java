7cd38be7fbea56e47aecdccc948046a67fd7f915
Koscee <52253099+Koscee@users.noreply.github.com>
2021-12-18 20:34:36 +0800

    Add user register endpoint, setup happy path and request body validation.

--- PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/services/UserService.java
+++ PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/services/UserService.java
@@ -1,11 +1,26 @@
 package com.xclusive.ppmtool.services;
 
+import com.xclusive.ppmtool.domain.User;
 import com.xclusive.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        // username has to be unique ( need Exception handler)
+        // Make sure that password and confirmPassword match
+        // We dont persist or show the confirmPassword
+
+        return userRepository.save(newUser);
+    }
 }
