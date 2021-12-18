102f3db285e590069da684e7b56b5d3e0951d7eb
Koscee <52253099+Koscee@users.noreply.github.com>
2021-12-18 21:10:07 +0800

    Handle exception when persisting data with username that already exist.

--- PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/services/UserService.java
+++ PersonalProjectManagementTool-Java/src/main/java/com/xclusive/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.xclusive.ppmtool.services;
 
 import com.xclusive.ppmtool.domain.User;
+import com.xclusive.ppmtool.exceptions.UsernameAlreadyExistException;
 import com.xclusive.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,12 +16,18 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser(User newUser) {
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        // username has to be unique ( need Exception handler)
-        // Make sure that password and confirmPassword match
-        // We dont persist or show the confirmPassword
+            // username has to be unique ( need Exception handler)
+            newUser.setUsername(newUser.getUsername());
 
-        return userRepository.save(newUser);
+            // Make sure that password and confirmPassword match
+            // We dont persist or show the confirmPassword
+
+            return userRepository.save(newUser);
+        } catch (Exception e) {
+            throw new UsernameAlreadyExistException("Username '"+newUser.getUsername()+"' already exist");
+        }
     }
 }
