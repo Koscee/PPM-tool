f24d62d6cec720b11018957dd5e140260c5d6f41
Koscee <52253099+Koscee@users.noreply.github.com>
2021-12-20 10:08:13 +0800

    Add security reducer and login action, store and decode token.

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,12 +1,38 @@
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+import setJwtToken from '../utils/setJwtToken';
 import dispatchAction from './dispatchHelper';
-import { GET_ERRORS, REMOVE_ERRORS } from './types';
+import { GET_ERRORS, REMOVE_ERRORS, SET_CURRENT_USER } from './types';
 
 export const createNewUser = (newUser, history) => async (dispatch) => {
   try {
     await axios.post('/api/users/register', newUser);
     history.push('/login');
     dispatchAction(dispatch, REMOVE_ERRORS, {});
+  } catch (error) {
+    dispatchAction(dispatch, GET_ERRORS, error.response.data);
+  }
+};
+
+export const login = (loginRequest) => async (dispatch) => {
+  try {
+    // send a post request
+    const res = await axios.post('/api/users/login', loginRequest);
+
+    // extract token from res.data
+    const { token } = res.data;
+
+    // store user's token in the localStorage
+    localStorage.setItem('jwtToken', token);
+
+    // set user's token in  the header
+    setJwtToken(token);
+
+    // decode token using jwt package library
+    const decodedToken = jwtDecode(token);
+
+    // dispatch info to securityReducer
+    dispatchAction(dispatch, SET_CURRENT_USER, decodedToken);
   } catch (error) {
     dispatchAction(dispatch, GET_ERRORS, error.response.data);
   }
